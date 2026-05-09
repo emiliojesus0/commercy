6884e047d1c6c9aded6338bf45200f6dc585009b
Emilio <emiliojesus0@gmail.com>
2026-05-09 03:48:07 -0500

    corrigiendo errores de conexion con DB

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,25 @@
 const mysql = require("mysql2");
 const env = require("./env");
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: env.dbHost,
   user: env.dbUser,
   password: env.dbPassword,
   database: env.dbName,
   port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-connection.connect((err) => {
+// prueba conexión
+pool.getConnection((err, connection) => {
   if (err) {
     console.log("Error de conexion a la base de datos:", err.message);
   } else {
     console.log("Conectado a la base de datos");
+    connection.release();
   }
 });
 
-module.exports = connection;
+module.exports = pool;
