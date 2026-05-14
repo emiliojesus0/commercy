4cf7f8893fc90b7044bd019f0849af7b9a176652
Emilio <emiliojesus0@gmail.com>
2026-05-14 15:17:01 -0500

    Acceso localhost

--- src/config/env.js
+++ src/config/env.js
@@ -31,4 +31,5 @@ module.exports = {
   cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
   cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
   cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
+  localUrl: process.env.LOCAL_URL,
 };
